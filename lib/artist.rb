934f4b8e328298d269ec58d89dab611a1c934f23
Mitchel Parish <mitchelparish@gmail.com>
2019-01-29 22:37:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
 class Artist
-  extend Memorable::ClassMethods
+  extend Memorable::ClassMethods, Findable 
   include Memorable::InstanceMethods
-  extend Findable 
+  extend Findable
   attr_accessor :name
   attr_reader :songs
 
